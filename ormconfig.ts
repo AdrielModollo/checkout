1a8537048e3fd715ec9738db2515d777a145d989
Adriel Modollo <adriel.modollo@lit.com.br>
2025-01-11 01:07:58 -0300

    fix: ormconfig

--- ormconfig.ts
+++ ormconfig.ts
@@ -1,6 +1,7 @@
 import { DataSource } from 'typeorm';
 import * as dotenv from 'dotenv';
 import mysql from 'mysql2';
+import { join } from 'path';
 
 dotenv.config();
 
@@ -12,8 +13,12 @@ const AppDataSource = new DataSource({
     username: process.env.DB_USERNAME || 'root',
     password: process.env.DB_PASSWORD || 'admin',
     database: process.env.DB_DATABASE || 'develcode',
-    entities: [__dirname + '/src/modules/entities/*.entity.{ts,js}'],
-    migrations: [__dirname + '/src/modules/migrations/*.{ts,js}'],
+    entities: [
+        join(__dirname, '**/entities/*.entity.{ts,js}'),
+    ],
+    migrations: [
+        join(__dirname, 'src/modules/migrations/*.{ts,js}')
+    ],
     synchronize: false,
 });
 
