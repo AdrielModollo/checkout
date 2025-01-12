23efaef46eb99bf995eb108c98da4c7abe4642c2
Adriel Modollo <adriel.modollo@lit.com.br>
2025-01-12 01:01:51 -0300

    refactor: order service

--- src/app/services/order.service.ts
+++ src/app/services/order.service.ts
@@ -2,18 +2,19 @@ import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
 import { Order } from '../entities/order.entity';
-import { RmqService } from '../config/rabbitMq/rabbitMq.service';
 import { logger } from '../communs/logger.winston';
+import { ConsumerAndSendQueueService } from './consumerAndSendQueue.service';
+import { CreateOrderDto } from '../dto/createOrder.dto';
 
 @Injectable()
 export class OrderService {
     constructor(
         @InjectRepository(Order)
         private readonly orderRepository: Repository<Order>,
-        private readonly rmqService: RmqService,
+        private readonly consumerAndSendQueueService: ConsumerAndSendQueueService,
     ) { }
 
-    async createOrder(createOrderDto): Promise<Order> {
+    async createOrder(createOrderDto: CreateOrderDto): Promise<Order> {
         try {
             const order = new Order();
             order.clientId = createOrderDto.clientId;
@@ -22,11 +23,13 @@ export class OrderService {
 
             const savedOrder = await this.orderRepository.save(order);
             logger.info(`Order created successfully: ${JSON.stringify(savedOrder)}`);
-            await this.rmqService.sendOrderStatus(order.id, order.status);
+
+            await this.consumerAndSendQueueService.sendOrderStatus(savedOrder.id, savedOrder.status);
 
             return savedOrder;
         } catch (error) {
             logger.error(`Error creating order: ${error.message}`);
+            throw error;
         }
     }
 }
